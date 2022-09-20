416335c41c55f75204cfa601ab2cb68841f680aa
Grober Balint <groberbalint@gmail.com>
2022-09-20 15:25:29 +0200

    load database

---
+++ Onlab2/Onlab2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using SigStat.Common;
+using SigStat.Common.Loaders;
+
+namespace Onlab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Svc2004Loader svc2004Loader = new Svc2004Loader(@"C:\BME\MSc\2.felev\onlab\SVC2004.zip", false);
+            IEnumerable<Signer> signers = svc2004Loader.EnumerateSigners();
+
+            foreach (var item in signers)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Onlab2/Onlab2/SimpleDTW.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onlab2
+{
+    public static class SimpleDTW
+    {
+
+        /*public static double CalculateDTW()
+        {
+
+        }*/
+
+    }
+}
