19e65f740779dc262f64b18a5e690ebe018ea49f
Gröber Bálint <groberbalint@gmail.com>
2022-10-02 20:57:29 +0200

    ImprovedDTW implementation

--- Onlab2/Onlab2/ImprovedDTW.cs
+++ Onlab2/Onlab2/ImprovedDTW.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SigStat.Common;
+using SigStat.Common.Algorithms.Distances;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,30 +11,33 @@ namespace Onlab2
     public static class ImprovedDTW
     {
 
-        public static double CalculateDTW<P>(IEnumerable<P> sequence1, IEnumerable<P> sequence2)
+        public static double CalculateDTW<P>(IEnumerable<P> sequence1, IEnumerable<P> sequence2, Func<P, P, double> distance, Func<P,P,P, double[]> difference)
         {
             var s1 = (new P[] { default(P) }).Concat(sequence1).ToArray();
             var s2 = (new P[] { default(P) }).Concat(sequence2).ToArray();
-        }
 
-        private static double Distance(double[] x, double[] y)
-        {
+            var n = s1.Length - 1;
+            var m = s2.Length - 1;
 
-        }
+            var dtw = new double[n + 1, m + 1];
+            dtw.SetValues(Double.PositiveInfinity);
+            dtw[0, 0] = 0;
 
-        private static double[] LocalDifference(double[] r)
-        {
-            double[] result = new double[r.Length + 1];
-            result[0] = 0;
-            for(int i = 1; i < r.Length; i++)
+            EuclideanDistance dist = new EuclideanDistance();
+
+            for (int i = 1; i <= n; i++)
             {
-                result[i] = ((r[i] - r[i - 1]) + ((r[i + 1] - r[i - 1]) / 2)) / 2;
+                for (int j = 1; j <= m; j++)
+                {
+                    var cost = distance(s1[i], s2[j]) * dist.Calculate(difference(s1[i - 1], s1[i], s1[i + 1]), difference(s2[j - 1], s2[j], s2[j + 1]));
+                    dtw[i, j] = cost;
+                }
             }
 
-            return result;
+            return dtw[n, m];
+
         }
-
-
+        
 
     }
 }
---
+++ Onlab2/Onlab2/LocalDistance.cs
@@ -0,0 +1,46 @@
+﻿using SigStat.Common.Algorithms.Distances;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onlab2
+{
+    public class LocalDistance : IDistance<double[]>
+    {
+        public double Calculate(double[] p1, double[] p2)
+        {
+            EuclideanDistance dist = new EuclideanDistance();
+            return dist.Calculate(p1, p2);
+
+        }
+
+
+        public double[] LocalDifference(double[] p1, double[] p2, double[] p3)
+        {
+            double[] p21 = new double[p1.Length];
+            for(int i = 0; i < p1.Length; i++)
+            {
+                p21[i] = p2[i] - p1[i];
+            }
+
+            double[] p31 = new double[p1.Length];
+            for(int i = 0; i < p1.Length; i++)
+            {
+                p31[i] = (p3[i] - p1[i]) / 2;
+            }
+
+            double[] diff = new double[p1.Length];
+
+            for(int i = 0; i < p1.Length; i++)
+            {
+                diff[i] = (p21[i] + p31[i]) / 2;
+            }
+
+            return diff;
+
+        }
+
+    }
+}
